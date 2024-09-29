92b334e4e489fb1a7bf9e1d3fccc528681425729
Ximena <moreraximena36@gmail.com>
2024-09-28 22:13:26 -0500

    se envian cambios de create

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -29,7 +29,10 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios= DB:: table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.new',['municipios'=>$municipios]);
     }
 
     /**
--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 <div class="container">
     <h1>Commune List</h1>
-    {{-- <a href="{{ route('comunas.create')}}" class="btn btn-success">Add </a> --}}
+    <a href="{{ route('comunas.create')}}" class="btn btn-success">Add </a>
     <table class="table">
         <thead>
           <tr>
@@ -20,15 +22,20 @@
           </tr>
         </thead>
         <tbody>
-            @forerach ($comunas as $comuna)
+            @foreach ($comunas as $comuna )
           <tr>
             <th scope="row">{{$comuna->comu_codi}}</th>
             <td>{{$comuna->comu_nomb}}</td>
             <td>{{$comuna->muni_nomb}}</td>
             <td><span> Actions</span></td>
           </tr>
-          
+          @endforeach
         </tbody>
       </table>
+      
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 </body>
 </html>
